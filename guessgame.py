739293924dc29ae20ad2fa9ad056cb3f50eb9f79
Manish.Jadhav <56465441+mail2manish@users.noreply.github.com>
2021-11-09 14:48:34 +0530

    Add files via upload

---
+++ guessgame.py
@@ -0,0 +1,26 @@
+#"Author :- Manish Prakash Jadhav"
+#"B.Tech Computer Science & Engineering At Daulatrao Aher College Of Engineering,Karad"
+#"Dated:- 09-11-2021"
+
+import random
+import time
+n=int(input("Enter Maximum Limit:- "))
+print("Numbers Will Be Generate Between 1 to {} :".format(n))
+generated_no=random.randint(1,n)
+guess_no=0
+while guess_no!=generated_no:
+    guess_no=int(input("Enter Your Guess...:-"))
+    if guess_no<generated_no:
+        print("Guessing...")
+        time.sleep(1)
+        print("Numebr Is Small")
+        
+    elif guess_no>generated_no:
+        print("Guessing...")
+        time.sleep(1)
+        print("Number Is Large")
+        
+    elif guess_no==generated_no:
+        print("Guessing...")
+        time.sleep(1)
+        print("Congratulation..! You Have Guessed Correct No")
